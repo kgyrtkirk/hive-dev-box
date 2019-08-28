dba58a8fc3193369fc246dd9eb62605d41ba3c4b
Zoltan Haindrich <kirk@rxd.hu>
2019-08-28 18:20:07 +0200

    fix psql default access

--- tools/install_psql.bash
+++ tools/install_psql.bash
@@ -3,3 +3,12 @@
 sudo apt-get install postgresql
 sudo -u postgres psql -c "CREATE USER hiveuser WITH PASSWORD 'mypassword'"
 sudo -u postgres createdb metastore -O hiveuser
+
+echo localhost:5432:metastore:hiveuser:mypassword > ~dev/.pgpass
+chmod 600 ~dev/.pgpass
+
+cat > /etc/profile.d/postgres_def.sh <<EOF
+export PGHOST=localhost
+export PGUSER=hiveuser
+export PGDATABASE=metastore
+EOF
