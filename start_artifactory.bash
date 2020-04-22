139e059e616980225bef33f3470ae19b40a4c712
Zoltan Haindrich <zhaindrich@cloudera.com>
2020-04-22 18:50:23 +0200

    use artifactory6

--- start_artifactory.bash
+++ start_artifactory.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-docker pull docker.bintray.io/jfrog/artifactory-oss:latest
+IMAGE=docker.bintray.io/jfrog/artifactory-oss:6.18.0
+
+docker pull $IMAGE
 
 NET=hive-dev-box-net
 docker network create hive-dev-box-net || true
 RUN_OPTS+=" --name artifactory"
 RUN_OPTS+=" --network $NET"
 RUN_OPTS+=" -d --restart always"
-RUN_OPTS+=" -v artifactory_data:/var/opt/jfrog/"
-RUN_OPTS+=" -v artifactory_data2:/opt/jfrog/"
+RUN_OPTS+=" -v artifactory_data:/var/opt/jfrog/artifactory"
 RUN_OPTS+=" -p 127.0.0.1:8081:8081"
-docker run $RUN_OPTS docker.bintray.io/jfrog/artifactory-oss:latest
-
+docker run $RUN_OPTS $IMAGE
